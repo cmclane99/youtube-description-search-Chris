29076624c7d1059bc45542c3f9177b6d655c77f2
Chris McLane <cmclane1@emich.edu>
2021-04-11 20:33:33 +0000

    Present the whoosh query results on the query webpage

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from youtube import search
-from description_search import create_whoosh_index
+from description_search import create_whoosh_index, query_on_whoosh
 
 print("The variable __name__: ")
 print(__name__)
@@ -22,16 +22,22 @@ def index(): # method name doesn't matter to Flask
 
     return render_template("index.html", user=username)
 
-@app.route("/query")
+@app.route("/query", methods=['GET', 'POST'])
 def query():
     arg = request.args.get('q')
     if not arg or not arg.strip():
         return render_template("query.html")
 
     index_name = "whoosh_index_" + arg
+	
+    if request.method == 'GET':
+        results = search(arg, 1)
+        create_whoosh_index(results, index_name)
+        return render_template("query.html", query_term=arg, data=results)
 
-    results = search(arg, 1)
-    create_whoosh_index(results, index_name)
-
-    return render_template("query.html", data=results)
-
+    if request.method == 'POST':
+        # the result sent by the search box on the query page
+        search_term = request.form['description_search']
+        print(search_term)
+        results = query_on_whoosh(index_name, search_term)
+        return render_template("query.html", query_term=arg, data=results)
